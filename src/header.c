1a4b8b7fd35006a29da2455ec2b1e263f8e06624
Tom Cosgrove <cryptocyclist@gmail.com>
2017-02-19 18:01:42 +0000

    Fix test for extended header
    Since '&' has lower precedence than '==', '==' will be evaluated first, and the original
    code was testing for the LSB set (flags & 1).  Just removing the equality test is the
    simplest fix.
    
    Found by clang.

--- src/header.c
+++ src/header.c
@@ -68,7 +68,7 @@ ID3v2_header* get_tag_header_with_buffer(char *buffer, int length)
     tag_header->flags = buffer[position += ID3_HEADER_REVISION];
     tag_header->tag_size = syncint_decode(btoi(buffer, ID3_HEADER_SIZE, position += ID3_HEADER_FLAGS));
 
-    if(tag_header->flags&(1<<6)==(1<<6))
+    if(tag_header->flags & (1 << 6))
     {
       // an extended header exists, so we retrieve the actual size of it and save it into the struct
       tag_header->extended_header_size = syncint_decode(btoi(buffer, ID3_EXTENDED_HEADER_SIZE, position += ID3_HEADER_SIZE));
